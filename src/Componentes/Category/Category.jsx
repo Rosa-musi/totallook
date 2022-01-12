f09b2e614205697143188d124403389a50e7d80b
Rosa-musi <nadamejor27@hotmail.com>
2022-01-11 18:15:52 -0600

    estilos

---
+++ src/Componentes/Category/Category.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './category.css'
+
+export default function Category() {
+  return (
+    < div id= "contentCategory">
+    <div id="imgCategory" ><h1 id= "nameCategory">Damas</h1></div>
+    <div id="imgCategory" ><h1 id= "nameCategory">Hombres</h1></div>
+    <div id="imgCategory" ><h1 id= "nameCategory">Hogar</h1></div>
+    <div id="imgCategory" ><h1 id= "nameCategory">Accesorios</h1></div>
+    <div id="imgCategory" ><h1 id= "nameCategory">Niños</h1></div>
+    </div>
+  );
+}
+
---
+++ src/Componentes/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React  from 'react'
+
+import './footer.css'
+
+export default function Footer() {
+    return(
+        <div className='footerContainer'>
+        <p className='footerText'>® 2022 Total Look, All rights reserved </p>
+    </div>
+    )
+}
