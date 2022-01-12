8fd362f2458c9f38a3e89398f89ce883c0219354
Gemma <gemminew@gmail.com>
2022-01-12 09:44:03 -0600

    login ya casi

--- src/Componentes/Header/Header.jsx
+++ src/Componentes/Header/Header.jsx
@@ -4,13 +4,14 @@ import './header.css'
 
 import logo from '../../assets/Logo.png'
 import shopCart from '../../assets/shopping-cart.png'
+import Login from '../../Login/Login'
 
 export default function Header(){
     return (
         <div className='navContainer'>
             <img className='navContainer_logo' src={logo} alt="logo"/>
             <div className='userContainer'>
-                <p className='sign'>Sign in</p> {/*renderizado condicional si está logueado o no */}
+                <p className='sign'>Sign in <Login></Login></p> {/*renderizado condicional si está logueado o no */}
                 <img className='userContainer_cartIcon' src={shopCart} alt="boton carrito compras"/>
             </div>
         </div>
